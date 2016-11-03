32bc0ea3bd165511d4af1ed8a0ba28ed786fb65c
Akash <akashkurdekar@gmail.com>
2016-11-03 23:42:22 +0000

    adding gorilla handlers for logging and compression

--- slack/cmd/main.go
+++ slack/cmd/main.go
@@ -6,19 +6,26 @@ import (
 	"log"
 	"fmt"
 	"time"
+	"strings"
+	"github.com/gorilla/handlers"
 )
 
 func main() {
+	r := http.NewServeMux()
 
-	http.HandleFunc("/pool-command", func (w http.ResponseWriter, r *http.Request) {
-		fmt.Println(r.URL.String())
-		fmt.Fprintf(w, "Hello, at %s", time.Now().Format(time.RFC3339))
-	});
+	RespondToPoolCommands := func (w http.ResponseWriter, r *http.Request) {
+		text := strings.ToLower(r.URL.Query().Get("text"))
+
+		fmt.Fprintf(w, "[%s] %s", time.Now().Format(time.RFC3339), text)
+	}
+
+	// Only log requests to our admin dashboard to stdout
+	r.Handle("/pool-command", handlers.LoggingHandler(os.Stdout, http.HandlerFunc(RespondToPoolCommands)))
 
 	port := os.Getenv("PORT")
 
 	fmt.Println("Starting on port ", port)
-	err := http.ListenAndServe(":" + port, nil)
+	err := http.ListenAndServe(":" + port, handlers.CompressHandler(r))
 	if err != nil {
 		log.Fatal("Error while starting the server", err)
 	}
